e36b623686a893da440bf8a2ff1a3bbd88fa83de
Howard Huang <hao_wen0626@hotmail.com>
2022-06-28 16:50:21 +0800

    建立一支轉址到 LINE Notify 進行授權的 API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.WebUtilities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("authorize", (IConfiguration configuration) =>
+{
+    const string authorizeEndpoint = "https://notify-bot.line.me/oauth/authorize";
+
+    var parameters = new Dictionary<string, string>()
+    {
+        ["response_type"] = "code",
+        ["client_id"] = configuration["Authentication:LineNotify:ClientId"],
+        ["redirect_uri"] = "https://localhost:44320/signin-line-notify",
+        ["scope"] = "notify",
+        ["state"] = "abcd"
+    };
+
+    var requestUri = QueryHelpers.AddQueryString(authorizeEndpoint, parameters);
+
+    return Results.Redirect(requestUri);
+});
 
 app.Run();
